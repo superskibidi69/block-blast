2e435cf4f9e04e739b0977f637720388112a9ea1
futzumi <102948087+futzumi@users.noreply.github.com>
2024-12-03 16:06:03 -0500

    Update game.js

--- game.js
+++ game.js
@@ -747,17 +747,21 @@ document.querySelector(".game-settings").onclick = function() {
         <button>X</button>
     </div>
     <div class="settings-section">
-        <div class="settings-option">
-            <div class="toggle-container" >
-                <span style="display:flex;"><svg fill="#fff" viewBox="0 0 24 24" id="sound-min" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="secondary" d="M17.54,16.54a1,1,0,0,1-.71-.3,1,1,0,0,1,0-1.41,4,4,0,0,0,0-5.66,1,1,0,1,1,1.41-1.41,6,6,0,0,1,0,8.48A1,1,0,0,1,17.54,16.54Z" style="fill: #fff;"></path><path id="primary" d="M13.38,4.08a1,1,0,0,0-1.09.21L8.59,8H6a2,2,0,0,0-2,2v4a2,2,0,0,0,2,2H8.59l3.7,3.71A1,1,0,0,0,13,20a.84.84,0,0,0,.38-.08A1,1,0,0,0,14,19V5A1,1,0,0,0,13.38,4.08Z" style="fill: #fff;"></path></g></svg>
+         <div class="settings-option">
+        <div class="toggle-container">
+            <span>
+                <svg fill="#fff" viewBox="0 0 24 24" id="sound-min" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
+                    <path d="M17.54,16.54a1,1,0,0,1-.71-.3,1,1,0,0,1,0-1.41,4,4,0,0,0,0-5.66,1,1,0,1,1,1.41-1.41,6,6,0,0,1,0,8.48A1,1,0,0,1,17.54,16.54Z" style="fill: #fff;"></path>
+                    <path d="M13.38,4.08a1,1,0,0,0-1.09.21L8.59,8H6a2,2,0,0,0-2,2v4a2,2,0,0,0,2,2H8.59l3.7,3.71A1,1,0,0,0,13,20a.84.84,0,0,0,.38-.08A1,1,0,0,0,14,19V5A1,1,0,0,0,13.38,4.08Z" style="fill: #fff;"></path>
+                </svg>
                 Sound
-                </span>
-                <label class="toggle">
-                    <input type="checkbox">
-                    <span class="toggle-slider"></span>
-                </label>
-            </div>
+            </span>
+            <label class="toggle">
+                <input type="checkbox">
+                <span class="toggle-slider"></span>
+            </label>
         </div>
+    </div>
         <div class="settings-option">
             <div class="toggle-container">
                 <span>
